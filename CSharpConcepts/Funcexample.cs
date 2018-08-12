98e70dc4efa3bc3dbfbdfb66ad6abe3c64adfb5e
Ashwin Kumar H <ashwinkpes@hotmail.com>
2018-08-12 18:20:21 +0530

    Func example

---
+++ CSharpConcepts/Funcexample.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConcepts
+{
+    internal class Funcexample
+    {
+        public static void FuncExample()
+        {
+            Console.WriteLine("Enter value between 1- 10");
+            string input = Console.ReadLine();
+
+            int number;
+            bool result = Int32.TryParse(input, out number);
+
+            if (result)
+            {
+                int res = GetThreeTimesNumber(number, DoubletheNumber);
+                Console.WriteLine($"Three times of the number is  {res}");
+            }            
+
+            //Example - 2 ---Func - Gets Reverse name.           
+            var reverseName = GetCompleteName(1, s => GetReverseName(s.fname, s.sname));
+            Console.WriteLine($"The Reverse Name is {reverseName}");
+
+            //Example - 3 ---Func - Gets complete name.            
+            ValidateUser();
+        }
+
+        private static int GetThreeTimesNumber(int userInput, Func<int, int> secondInput)
+        {
+            return userInput + DoubletheNumber(userInput);
+        }
+
+        private static int DoubletheNumber(int firstNumber)
+        {
+            return firstNumber * 2;
+        }
+
+        private static string GetCompleteName(int id, Func<student, string> fullName)
+        {
+            string completename = string.Empty;
+            if (id == 1)
+            {
+                completename = fullName(new student { fname = "Ashwin", sname = "Kumar" });
+            }
+
+            return completename;
+        }
+
+        private static string GetReverseName(string fname, string sname)
+        {
+
+            return (sname + " " + fname).ToUpper();
+        }
+
+        public class student
+        {
+            public string fname { get; set; }
+            public string sname { get; set; }
+        }
+
+        private static void ValidateUser()
+        {
+            Func<string, string, string> concatenateuser = (x, y) => x + y;
+            GetConcatenatedName(concatenateuser);
+        }
+
+        private static void GetConcatenatedName(Func<string, string, string> getFullName)
+        {
+            Console.WriteLine($"The complete name is {getFullName("Ashwin", "Kumar.H")}");
+        }
+    }
+}
--- CSharpConcepts/Program.cs
+++ CSharpConcepts/Program.cs
@@ -6,7 +6,31 @@ namespace CSharpConcepts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Showmenu();
+            }
         }
+
+        private static void Showmenu()
+        {
+            Console.WriteLine("Select task to perform...");
+            Console.WriteLine("Enter 1 for Func");
+            Console.WriteLine("Enter 2 for Action");
+            Console.WriteLine("Enter 0 for exit");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            switch(keyInfo.Key)
+            {
+                case ConsoleKey.D1: Console.WriteLine("1 was pressed , 3 examples of Func will get executed"); Funcexample.FuncExample(); break;
+                case ConsoleKey.D2: Console.WriteLine("2 was pressed"); break;
+                case ConsoleKey.D0: Console.WriteLine("0 was pressed"); Environment.Exit(-1); break;
+                default: Console.WriteLine("Enter valid input!!!"); break;
+            }
+        }
+
+        
+        
     }
 }
